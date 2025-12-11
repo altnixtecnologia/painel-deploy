0286ac1cfe99d4ec02c0a7475acac29c14893982
EEB Getulio Vargas <orientacao.getuliovargas@sed.sc.gov.br>
2025-12-11 17:05:01 -0300

    Atualizando Painel

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"b5990e5ccc5e325fd24f0746e7d6689bbebc7c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4023325334"
+    serviceWorkerVersion: "1911349844"
   }
 });
