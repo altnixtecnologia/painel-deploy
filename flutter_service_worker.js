cb85629c3fb57cd1b45da47baaf18f9b4710a6b6
EEB Getulio Vargas <orientacao.getuliovargas@sed.sc.gov.br>
2025-12-13 15:11:04 -0300

    Atualizando Geral

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -25,14 +25,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "5cc31da7b4bd96305db124f8d5703236",
+"flutter_bootstrap.js": "c2554ccb9d8b75e567ff3e8f8ef11b00",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "230c9552d49c62e6f107fd39e50da287",
 "/": "230c9552d49c62e6f107fd39e50da287",
-"main.dart.js": "8779dd0fbfea82b48233d0f41659f47e",
+"main.dart.js": "ea597e6fd1798890a52ec08fe242e69a",
 "manifest.json": "80d96e108661e636039464984a28f8c8",
 "version.json": "40d28f25274473f8d575b06785238028"};
 // The application shell files that are downloaded before a service worker can
