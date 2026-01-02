fffd976a7044f4d50b3c0b78622080382d2aae15
EEB Getulio Vargas <orientacao.getuliovargas@sed.sc.gov.br>
2026-01-01 21:14:59 -0300

    Atualizando novo

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"b5990e5ccc5e325fd24f0746e7d6689bbebc7c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3540219487"
+    serviceWorkerVersion: "3280008378"
   }
 });
